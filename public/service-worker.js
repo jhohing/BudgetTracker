b78d8e1146679900ebefa2b3cb0d2813ac3f68dc
Justin Hohing <jnh0627@yahoo.com>
2021-04-24 13:49:18 -0400

    edited index.html, service-worker, and manifest files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,64 +1,78 @@
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/assets/css/style.css',
-    '/assets/js/index.js',
-    '/assets/js/db.js',
-    '/assets/icons/icon-192x192.png',
-    '/assets/icons/icon-512x512.png',
+    "/",
+    "/index.html",
+    "/assets/css/styles.css",
+    "/assets/js/index.js",
+    "/assets/js/db.js",
+    "/assets/icons/icon-192x192.png",
+    "/assets/icons/icon-512x512.png",
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
   ];
   
-  const PRECACHE = 'precache-v1';
-  const RUNTIME = 'runtime';
+  const CACHE_NAME = "static-cache-v1";
+  const DATA_CACHE_NAME = "data-cache-v1";
   
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches
-        .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
+  self.addEventListener("install", (evt) => {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
     );
+  
+    self.skipWaiting();
   });
   
-  // The activate handler takes care of cleaning up old caches.
-  self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-      caches
-        .keys()
-        .then((cacheNames) => {
-          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-        })
-        .then((cachesToDelete) => {
-          return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
-              return caches.delete(cacheToDelete);
-            })
-          );
-        })
-        .then(() => self.clients.claim())
+  self.addEventListener("activate", (evt) => {
+    // remove old caches
+    evt.waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              return caches.delete(key);
+            }
+          })
+        );
+      })
     );
+  
+    self.clients.claim();
   });
   
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
-      event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
-          }
+  self.addEventListener("fetch", (evt) => {
+    // cache successful GET requests to the API
+    if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
+      evt.respondWith(
+        caches
+          .open(DATA_CACHE_NAME)
+          .then((cache) => {
+            return fetch(evt.request)
+              .then((response) => {
+                // If the response was good, clone it and store it in the cache.
+                if (response.status === 200) {
+                  cache.put(evt.request, response.clone());
+                }
   
-          return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
                 return response;
+              })
+              .catch(() => {
+                // Network request failed, try to get it from the cache.
+                return cache.match(evt.request);
               });
-            });
-          });
-        })
+          })
+          .catch((err) => console.log(err))
       );
-    }
-  });
   
+      // stop execution of the fetch event callback
+      return;
+    }
+  
+    // if the request is not for the API, serve static assets using
+    // "offline-first" approach.
+    evt.respondWith(
+      caches.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      })
+    );
+  });
