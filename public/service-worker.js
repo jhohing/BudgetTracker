1d60da2fae5bfaf65209d8d8bd986008820af37b
Justin Hohing <jnh0627@yahoo.com>
2021-04-24 12:06:11 -0400

    missing db.js file cache in service-worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,7 @@ const FILES_TO_CACHE = [
     '/index.html',
     '/assets/css/style.css',
     '/assets/js/index.js',
+    '/assets/js/db.js',
     '/assets/icons/icon-192x192.png',
     '/assets/icons/icon-512x512.png',
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
